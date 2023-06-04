214b96f99c7334baf9bf99b6bb787cef71fedd82
Dustin Franklin <df59@zips.uakron.edu>
2023-06-04 01:50:53 -0400

    Remove unnecessary AOE2 layers

--- keyboards/bastardkb/charybdis/4x6/keymaps/dfranklint/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/dfranklint/keymap.c
@@ -42,10 +42,7 @@ enum charybdis_keymap_layers {
     LAYER_NUM,
     LAYER_SEL,
     LAYER_AOE2_BASE,
-    LAYER_AOE2_GROUPSELECT,
-    LAYER_AOE2_GROUPCREATE,
-    LAYER_AOE2_GROUPAPPEND,
-    LAYER_AOE2_CYCLE
+    LAYER_AOE2_2
 };
 
 // clang-format off
@@ -110,72 +107,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [LAYER_AOE2_BASE] = LAYOUT(
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
-        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, TG(LAYER_AOE2_BASE),
+        KC_ESC,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX, TG(LAYER_AOE2_BASE),
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        XXXXXXX,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+        KC_TILD,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        MO(LAYER_AOE2_GROUPAPPEND),KC_A,KC_S,KC_D,KC_F,KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_BSPC, KC_DEL,
+        MO(LAYER_AOE2_2),KC_A,KC_S,  KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_BSPC, KC_DEL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        XXXXXXX,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_BTN1,  KC_BTN2, DRGSCRL, KC_LALT,
+        KC_DEL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_BTN1,  KC_BTN2, DRGSCRL, KC_LALT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-     KC_LSFT, MO(LAYER_AOE2_CYCLE),MO(LAYER_AOE2_GROUPSELECT),     MO(LAYER_NUM),  KC_SPC,
-                                           KC_LALT, KC_LCTL,       LT(LAYER_NAV, KC_ENT)
+                              KC_LSFT, KC_LCTL, RCS(XXXXXXX),     MO(LAYER_NUM),  KC_SPC,
+                                       KC_LALT, RSA(XXXXXXX),     LT(LAYER_NAV, KC_ENT)
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
-  [LAYER_AOE2_GROUPSELECT] = LAYOUT(
+  [LAYER_AOE2_2] = LAYOUT(
   // ╭────────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────────╮
-        XXXXXXX,   RCS(KC_1),  RCS(KC_2),  RCS(KC_3),  RCS(KC_4),  RCS(KC_5),            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
   // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   RCS(KC_6),  RCS(KC_7),  RCS(KC_8),  RCS(KC_9),  RCS(KC_F10),          XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        KC_F7,     KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
   // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   MEH(KC_1),  MEH(KC_2),  MEH(KC_3),  MEH(KC_4),  MEH(KC_5),            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
   // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   MEH(KC_6),  MEH(KC_7),  MEH(KC_8),  MEH(KC_9),  MEH(KC_F10),          XXXXXXX,    XXXXXXX,    KC_BTN1,    KC_BTN2,    DRGSCRL,    XXXXXXX,
+        KC_N,      KC_G,       KC_H,       KC_J,       KC_K,       KC_L,                 XXXXXXX,    XXXXXXX,    KC_BTN1,    KC_BTN2,    DRGSCRL,    XXXXXXX,
   // ╰────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────╯
-                                                       KC_LSFT, XXXXXXX,  XXXXXXX,       MO(LAYER_NUM),  KC_SPC,
-                                             KC_LALT,  MO(LAYER_AOE2_GROUPCREATE),       LT(LAYER_NAV, KC_ENT)
-  //                                           ╰──────────────────────────────────╯ ╰────────────────────────────────╯
-  ),
-  [LAYER_AOE2_GROUPCREATE] = LAYOUT(
-  // ╭────────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────────╮
-        XXXXXXX,   LCTL(KC_1), LCTL(KC_2), LCTL(KC_3), LCTL(KC_4), LCTL(KC_5),           XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   LCTL(KC_6), LCTL(KC_7), LCTL(KC_8), LCTL(KC_9), LCTL(KC_0),           XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   LCA(KC_1),  LCA(KC_2),  LCA(KC_3),  LCA(KC_4),  LCA(KC_5),            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   LCA(KC_6),  LCA(KC_7),  LCA(KC_8),  LCA(KC_9),  LCA(KC_0),            XXXXXXX,    XXXXXXX,    KC_BTN1,    KC_BTN2,    DRGSCRL,    XXXXXXX,
-  // ╰────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────╯
-                                                       KC_LSFT, XXXXXXX,  XXXXXXX,       MO(LAYER_NUM),  KC_SPC,
-                                                                KC_LALT,  KC_LCTL,       LT(LAYER_NAV, KC_ENT)
-  //                                           ╰──────────────────────────────────╯ ╰────────────────────────────────╯
-  ),
-  [LAYER_AOE2_GROUPAPPEND] = LAYOUT(
-  // ╭────────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────────╮
-        XXXXXXX,   LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),           XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0),           XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   LSA(KC_1),  LSA(KC_2),  LSA(KC_3),  LSA(KC_4),  LSA(KC_5),            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   LSA(KC_6),  LSA(KC_7),  LSA(KC_8),  LSA(KC_9),  LSA(KC_0),            XXXXXXX,    XXXXXXX,    KC_BTN1,    KC_BTN2,    DRGSCRL,    XXXXXXX,
-  // ╰────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────╯
-                                                       KC_LSFT, XXXXXXX,  XXXXXXX,       MO(LAYER_NUM),  KC_SPC,
-                                                                KC_LALT,  KC_LCTL,       LT(LAYER_NAV, KC_ENT)
-  //                                           ╰──────────────────────────────────╯ ╰────────────────────────────────╯
-  ),
-  [LAYER_AOE2_CYCLE] = LAYOUT(
-  // ╭────────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────────╮
-        XXXXXXX,   LCTL(KC_N), LCTL(KC_T), LCA(KC_COMM), LCTL(KC_D), LCTL(KC_M),         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        LALT(KC_N),LCTL(KC_B), LCTL(KC_A), LCTL(KC_L),   LCTL(KC_K), LCTL(KC_Y),         XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        LALT(KC_C),LCTL(KC_G), KC_SPC,     KC_H,         KC_DOT,     KC_COMM,            XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-  // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        LALT(KC_R),LCTL(KC_Z), LCTL(KC_I), LCTL(KC_V),   LCTL(KC_S), LCTL(KC_U),         XXXXXXX,    XXXXXXX,    KC_BTN1,    KC_BTN2,    DRGSCRL,    XXXXXXX,
-  // ╰────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────╯
-                                                       XXXXXXX, XXXXXXX,  XXXXXXX,       MO(LAYER_NUM),  KC_SPC,
-                                                                KC_LSFT,  XXXXXXX,       LT(LAYER_NAV, KC_ENT)
+                                                KC_LSFT, KC_LCTL, RCS(XXXXXXX),         MO(LAYER_NUM),  KC_SPC,
+                                                         KC_LALT, RSA(XXXXXXX),         LT(LAYER_NAV, KC_ENT)
   //                                           ╰──────────────────────────────────╯ ╰────────────────────────────────╯
   ),
   [LAYER_SEL] = LAYOUT(
@@ -216,17 +171,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
           case LAYER_AOE2_BASE:
                rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_aoe2_base_effect);
                break;
-          case LAYER_AOE2_GROUPSELECT:
-               rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_aoe2_groupselect_effect);
-               break;
-          case LAYER_AOE2_GROUPCREATE:
-               rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_aoe2_groupcreate_effect);
-               break;
-          case LAYER_AOE2_GROUPAPPEND:
-               rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_aoe2_groupappend_effect);
-               break;
-          case LAYER_AOE2_CYCLE:
-               rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_aoe2_cycle_effect);
+          case LAYER_AOE2_2:
+               rgb_matrix_mode_noeeprom(RGB_MATRIX_CUSTOM_aoe2_2_effect);
                break;
     }
      #endif // RGB_MATRIX_ENABLE
