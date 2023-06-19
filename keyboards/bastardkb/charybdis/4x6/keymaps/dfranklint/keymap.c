c013972db728c8e4dcb5627891fb7d2156ddcdd0
Dustin Franklin <df59@zips.uakron.edu>
2023-06-19 10:32:40 -0400

    Modify aoe2 layers

--- keyboards/bastardkb/charybdis/4x6/keymaps/dfranklint/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/dfranklint/keymap.c
@@ -109,28 +109,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ╭──────────────────────────────────────────────────────╮ ╭──────────────────────────────────────────────────────╮
         KC_ESC,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX, TG(LAYER_AOE2_BASE),
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_TILD,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, XXXXXXX,
+        KC_TILD,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        MO(LAYER_AOE2_2),KC_A,KC_S,  KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_BSPC, KC_DEL,
+        KC_TAB,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,       KC_H,    KC_J,    KC_K,    KC_L, KC_BSPC, KC_DEL,
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
-        KC_DEL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_BTN1,  KC_BTN2, DRGSCRL, KC_LALT,
+        KC_DEL,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_BTN1,  KC_BTN2, DRGSCRL, KC_DOT,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                              KC_LSFT, KC_LCTL, RCS(XXXXXXX),     MO(LAYER_NUM),  KC_SPC,
-                                       KC_LALT, RSA(XXXXXXX),     LT(LAYER_NAV, KC_ENT)
+                              KC_LSFT, KC_LCTL, KC_LALT,           MO(LAYER_AOE2_2),  KC_SPC,
+                                   RCS(XXXXXXX), RSA(XXXXXXX),     KC_ENT
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
   [LAYER_AOE2_2] = LAYOUT(
   // ╭────────────────────────────────────────────────────────────────────────────╮ ╭────────────────────────────────────────────────────────────────────────────╮
         KC_F1,     KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F6,                XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
   // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        KC_F7,     KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,               XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        KC_F7,     KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,               XXXXXXX,    KC_7,    KC_8,    KC_9,    XXXXXXX,    XXXXXXX,
   // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        XXXXXXX,   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,                 XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        XXXXXXX,   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,                 XXXXXXX,    KC_4,    KC_5,    KC_6,    XXXXXXX,    XXXXXXX,
   // ├────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────┤
-        KC_N,      KC_G,       KC_H,       KC_J,       KC_K,       KC_L,                 XXXXXXX,    XXXXXXX,    KC_BTN1,    KC_BTN2,    DRGSCRL,    XXXXXXX,
+        KC_N,      KC_G,       KC_H,       KC_J,       KC_K,       KC_L,                 KC_0,       KC_1,    KC_2,    KC_3,    DRGSCRL,    XXXXXXX,
   // ╰────────────────────────────────────────────────────────────────────────────┤ ├────────────────────────────────────────────────────────────────────────────╯
-                                                KC_LSFT, KC_LCTL, RCS(XXXXXXX),         MO(LAYER_NUM),  KC_SPC,
-                                                         KC_LALT, RSA(XXXXXXX),         LT(LAYER_NAV, KC_ENT)
+                                                       KC_LSFT, KC_LCTL, KC_LALT,        XXXXXXX,  KC_SPC,
+                                                       RCS(XXXXXXX), RSA(XXXXXXX),     KC_ENT
   //                                           ╰──────────────────────────────────╯ ╰────────────────────────────────╯
   ),
   [LAYER_SEL] = LAYOUT(
